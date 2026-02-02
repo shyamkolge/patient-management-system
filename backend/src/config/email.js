b52f96dc563191f0eb2ad901b3b9b2c54d223b8e
Shyam Kolge <shyamnkolge2020@gmail.com>
2026-02-02 19:39:51 +0530

    email chnages

--- backend/src/config/email.js
+++ backend/src/config/email.js
@@ -17,9 +17,6 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
     },
-    tls: {
-        rejectUnauthorized: false
-    }
 });
 
 
